af07d43091e122531d2481293a6faaf1b3973f86
Cristofer Figueroa <cristo.figueroam@duocuc.cl>
2026-05-11 21:41:57 -0400

    Add files via upload

---
+++ MS-Repuestos/MS-Repuestos/src/main/java/com/AutoMaster/MS_Repuestos/MsRepuestosApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Repuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRepuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRepuestosApplication.class, args);
+	}
+
+}
---
+++ MS-Repuestos/MS-Repuestos/src/test/java/com/AutoMaster/MS_Repuestos/MsRepuestosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoMaster.MS_Repuestos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRepuestosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
